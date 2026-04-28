90b091be6ae1a58064fee1fa1ee607bedc3e3f15
amaturanaa <alej.maturana@duocuc.cl>
2026-04-28 19:11:52 -0400

    feat: proyecto raíz casino-alimentos inicializado

---
+++ src/main/java/com/casino/CasinoAlimentosApplication.java
@@ -0,0 +1,13 @@
+package com.casino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasinoAlimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasinoAlimentosApplication.class, args);
+	}
+
+}
