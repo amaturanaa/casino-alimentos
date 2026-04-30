b6b0d1bb762308cdc6756bdf239cd30a263b15b9
amaturanaa <alej.maturana@duocuc.cl>
2026-04-29 22:31:08 -0400

    "feat: se agrega modulo sucursales"

---
+++ ms-sucursales/src/test/java/com/casino/mssucursales/MsSucursalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casino.mssucursales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSucursalesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
