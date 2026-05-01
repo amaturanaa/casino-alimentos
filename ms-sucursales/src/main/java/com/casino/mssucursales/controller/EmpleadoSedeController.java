9feab92db14bd703994caabf3e1260f115f7af37
amaturanaa <alej.maturana@duocuc.cl>
2026-05-01 12:10:57 -0400

    "feat: endpoints SedeCasino funcionando y probados en Postman"

--- ms-sucursales/src/main/java/com/casino/mssucursales/controller/EmpleadoSedeController.java
+++ ms-sucursales/src/main/java/com/casino/mssucursales/controller/EmpleadoSedeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/empleado_sede")
+@RequestMapping("/api/empleado-sede")
 @RequiredArgsConstructor
 public class EmpleadoSedeController {
 
