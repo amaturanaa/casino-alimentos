6eb7b6a45b5ce496ba6ae3d7c1d032ab1058ca35
amaturanaa <alej.maturana@duocuc.cl>
2026-05-18 21:02:21 -0400

    feat: agregar dependencia Flyway en todos los microservicios con migraciones deshabilitadas

--- ms-reservas/src/main/java/com/casino/msreservas/service/ReservaService.java
+++ ms-reservas/src/main/java/com/casino/msreservas/service/ReservaService.java
@@ -5,12 +5,20 @@ import com.casino.msreservas.dto.ReservaResponseDTO;
 import java.util.List;
 
 public interface ReservaService {
+
     ReservaResponseDTO crear(ReservaRequestDTO dto);
+
     ReservaResponseDTO obtenerPorId(Long id);
+
     List<ReservaResponseDTO> listarPorUsuario(Long usuarioId);
+
     List<ReservaResponseDTO> listarPorTurno(Long turnoId);
+
     List<ReservaResponseDTO> listarPorSede(Long sedeId);
+
     List<ReservaResponseDTO> listarPorEstado(String estado);
+
     ReservaResponseDTO cancelar(Long id);
+
     List<ReservaResponseDTO> listar();
 }
